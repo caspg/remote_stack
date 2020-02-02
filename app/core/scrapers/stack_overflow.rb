0b26455051b7ce624d8f1c055e8cc5760a2ce3be
Kacper Golinski <kacper.golinski@gmail.com>
2020-02-02 19:54:57 +0100

    create ScrapJobDetails

--- app/core/scrapers/stack_overflow.rb
+++ app/core/scrapers/stack_overflow.rb
@@ -2,18 +2,8 @@ module Scrapers
   module StackOverflow
     class << self
       def scrap_data
-        puts REMOTE_JOBS_URL
-
-        # fetch all urls
+        # parse rss and return feed items
         # open each page and scrap data
-      end
-
-      private
-
-      def fetch_urls
-        # fetch urls from that page
-        # should I go to the next page?
-        # return all links
       end
     end
   end
---
+++ app/core/scrapers/stack_overflow/scrap_job_details.rb
@@ -0,0 +1,58 @@
+module Scrapers
+  module StackOverflow
+    class ScrapJobDetails
+      ScrapedJobDetails = Struct.new(
+        :id,
+        :salary,
+        :company,
+        :benefits,
+        keyword_init: true,
+      )
+
+      def initialize(rss_feed_item:)
+        @rss_feed_item = rss_feed_item
+      end
+
+      def call
+        ScrapedJobDetails.new(
+          id: rss_feed_item.id,
+          salary: salary,
+          company: company,
+          benefits: benefits,
+        )
+      end
+
+      private
+
+      attr_reader :rss_feed_item
+
+      def document
+        @document ||= begin
+          # TODO(kacper): maybe check if returns 200
+          raw_document = Net::HTTP.get(URI(rss_feed_item.link))
+          Nokogiri::HTML(raw_document)
+        end
+      end
+
+      def salary
+        document.css('.-salary')
+                .first
+                &.content
+                &.strip
+      end
+
+      def company
+        document.css('header [href^="/jobs/companies/"]')
+                .find { |i| i.content.present? }
+                &.content
+      end
+
+      def benefits
+        document.css('.-benefits')
+                .first
+                &.content
+                &.strip
+      end
+    end
+  end
+end
