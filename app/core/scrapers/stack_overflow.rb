3a34fd9162394f96634b0cb692d361c1a5c343ea
Kacper Golinski <kacper.golinski@gmail.com>
2020-02-02 20:17:59 +0100

    Update stack_overflow.rb

--- app/core/scrapers/stack_overflow.rb
+++ app/core/scrapers/stack_overflow.rb
@@ -1,9 +1,25 @@
 module Scrapers
   module StackOverflow
     class << self
-      def scrap_data
-        # parse rss and return feed items
-        # open each page and scrap data
+      def scrap_data(last_parsed_id)
+        parsed_rss_feed_items(last_parsed_id).map do |rss_feed_item|
+          [
+            rss_feed_item,
+            scrap_job_details(rss_feed_item)
+          ]
+        end
+      end
+
+      private
+
+      def parsed_rss_feed_items(last_parsed_id)
+        ::Scrapers::StackOverflow::ParseRss.new(last_parsed_id: last_parsed_id).call
+      end
+
+      def scrap_job_details(rss_feed_item)
+        sleep(1)
+
+        ::Scrapers::StackOverflow::ScrapJobDetails.new(rss_feed_item: rss_feed_item).call
       end
     end
   end
