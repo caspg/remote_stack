41e44bb063ac19528a1c9e90f0ab6bef982e64f2
Kacper Golinski <kacper.golinski@gmail.com>
2020-02-02 21:26:50 +0100

    create Skills::FindOrCreateSkills

---
+++ app/core/skills/find_or_create_skills.rb
@@ -0,0 +1,31 @@
+module Skills
+  class FindOrCreateSkills
+    def initialize(skill_names:)
+      @skill_names = skill_names
+    end
+
+    def call
+      existing_skills + created_skills
+    end
+
+    private
+
+    attr_reader :skill_names
+
+    def existing_skills
+      @existing_skills ||= Skill.where(name: skill_names)
+    end
+
+    def existing_skill_names
+      existing_skills.map(&:name)
+    end
+
+    def created_skills
+      (skill_names - existing_skill_names).map do |skill_name|
+        puts "skill_name: #{skill_name}"
+
+        Skill.create!(name: skill_name)
+      end
+    end
+  end
+end
