7132bce7b38907a9b60f3ba484bfa5cda2191007
Kacper Golinski <kacper.golinski@gmail.com>
2020-02-04 15:52:15 +0100

    create we_work_remotely/parse_rss.rb

---
+++ app/core/scrapers/we_work_remotely/parse_rss.rb
@@ -0,0 +1,27 @@
+require 'rss'
+
+module Scrapers
+  module WeWorkRemotely
+    class ParseRss < ::Scrapers::RssParser
+      RSS_FEED_URL = 'https://weworkremotely.com/categories/remote-programming-jobs.rss'.freeze
+
+      def initialize(last_origin_id:)
+        super(last_guid: last_origin_id)
+      end
+
+      def call
+        super
+      end
+
+      private
+
+      def item_id(item)
+        Digest::MD5.hexdigest(item.guid.content)
+      end
+
+      def rss_feed_url
+        RSS_FEED_URL
+      end
+    end
+  end
+end
