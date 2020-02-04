842263e0070b8ab5c63709c5f292fabf2bdce55b
Kacper Golinski <kacper.golinski@gmail.com>
2020-02-04 15:52:03 +0100

    refactor stack_overflow/parse_rss.rb and create rss_parser

---
+++ app/core/scrapers/rss_parser.rb
@@ -0,0 +1,48 @@
+module Scrapers
+  class RssParser
+    def initialize(last_guid:)
+      @last_guid = last_guid
+    end
+
+    def call
+      items.map do |item|
+        ::Scrapers::RssFeedItem.new(
+          id: item_id(item),
+          title: item.title,
+          link: item.link,
+          description: item.description,
+          categories: item.categories.map(&:content),
+          publication_datetime: item.pubDate.to_datetime,
+        )
+      end
+    end
+
+    private
+
+    attr_reader :last_guid
+
+    def item_id(item)
+      item.guid.content
+    end
+
+    def items
+      sorted_items.take_while do |item|
+        item.guid.content.to_s != last_guid
+      end
+    end
+
+    def sorted_items
+      feed.items.sort_by(&:pubDate).reverse
+    end
+
+    def feed
+      # TODO(kacper): check if all good here
+      url_content = Net::HTTP.get(URI(rss_feed_url))
+      ::RSS::Parser.parse(url_content)
+    end
+
+    def rss_feed_url
+      raise 'Missing feed url'
+    end
+  end
+end
--- app/core/scrapers/stack_overflow/parse_rss.rb
+++ app/core/scrapers/stack_overflow/parse_rss.rb
@@ -2,43 +2,21 @@ require 'rss'
 
 module Scrapers
   module StackOverflow
-    class ParseRss
+    class ParseRss < ::Scrapers::RssParser
       RSS_FEED_URL = 'https://stackoverflow.com/jobs/feed?r=true'.freeze
 
       def initialize(last_origin_id:)
-        @last_origin_id = last_origin_id.to_s
+        super(last_guid: last_origin_id)
       end
 
       def call
-        items.map do |item|
-          ::Scrapers::RssFeedItem.new(
-            id: item.guid.content,
-            title: item.title,
-            link: item.link,
-            description: item.description,
-            categories: item.categories.map(&:content),
-            publication_datetime: item.pubDate.to_datetime,
-          )
-        end
+        super
       end
 
       private
 
-      attr_reader :last_origin_id
-
-      def items
-        sorted_items.take_while do |item|
-          item.guid.content.to_s != last_origin_id
-        end
-      end
-
-      def sorted_items
-        feed.items.sort_by(&:pubDate).reverse
-      end
-
-      def feed
-        url_content = Net::HTTP.get(URI(RSS_FEED_URL))
-        ::RSS::Parser.parse(url_content)
+      def rss_feed_url
+        RSS_FEED_URL
       end
     end
   end
