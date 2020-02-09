1f6a6cef828fc209a386a42cff46adddc865b248
Kacper Golinski <kacper.golinski@gmail.com>
2020-02-09 20:36:39 +0100

    create home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   def index
+    @job_posts = home_page_job_posts
+  end
+
+  private
+
+  def home_page_job_posts
+    JobPost
+      .where('publication_datetime > ?', 30.days.ago)
+      .order(publication_datetime: :desc)
   end
 end
