9e82a1c0219a752cb75a52662709c60968e7b1a9
Kacper Golinski <kacper.golinski@gmail.com>
2020-02-17 09:08:42 +0100

    Update sidekiq.rb

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,5 @@
 schedule_file = 'config/schedule.yml'
 
 if File.exist?('config/schedule.yml') && !Rails.env.test?
-  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+  Sidekiq::Cron::Job.load_from_hash!(YAML.load_file(schedule_file))
 end
