8698351d6d0e5e8e274674738d663d94697f9e9b
Kacper Golinski <kacper.golinski@gmail.com>
2020-02-17 14:45:01 +0100

    add `fresh_when` to job posts show action

--- app/controllers/job_posts_controller.rb
+++ app/controllers/job_posts_controller.rb
@@ -1,5 +1,7 @@
 class JobPostsController < ApplicationController
   def show
     @job_post = JobPost.find(params[:id])
+
+    fresh_when(etag: @job_post, last_modified: @job_post.updated_at.utc, public: true)
   end
 end
